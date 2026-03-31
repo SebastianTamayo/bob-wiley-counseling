a0e6feb4c23edd472c36b75bbfa8e68368546f12
SebastianTamayo <intuitivexp@gmail.com>
2026-03-30 14:54:06 -1000

    fix: suppress body hydration warnings caused by browser extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
-      <body>
+      <body suppressHydrationWarning>
         <Navbar />
         {children}
         <Footer />
